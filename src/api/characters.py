3defb6c01e5acd0e5caa49b97d7a3c251552f46f
DillonRego <dillonjrego17@gmail.com>
2023-05-01 16:09:31 -0700

    begin implementation

--- src/api/characters.py
+++ src/api/characters.py
@@ -1,29 +1,10 @@
 from fastapi import APIRouter, HTTPException
 from enum import Enum
 from collections import Counter
-
 from fastapi.params import Query
 from src import database as db
 
 router = APIRouter()
-
-
-def get_top_conv_characters(character):
-    c_id = character.id
-    movie_id = character.movie_id
-    all_convs = filter(
-        lambda conv: conv.movie_id == movie_id
-        and (conv.c1_id == c_id or conv.c2_id == c_id),
-        db.conversations.values(),
-    )
-    line_counts = Counter()
-
-    for conv in all_convs:
-        other_id = conv.c2_id if conv.c1_id == c_id else conv.c1_id
-        line_counts[other_id] += conv.num_lines
-
-    return line_counts.most_common()
-
 
 @router.get("/characters/{id}", tags=["characters"])
 def get_character(id: int):
@@ -46,35 +27,21 @@ def get_character(id: int):
     * `number_of_lines_together`: The number of lines the character has with the
       originally queried character.
     """
+#     conn = engine.connect()
 
-    character = db.characters.get(id)
-
-    if character:
-        movie = db.movies.get(character.movie_id)
-        result = {
-            "character_id": character.id,
-            "character": character.name,
-            "movie": movie and movie.title,
-            "gender": character.gender,
-            "top_conversations": (
-                {
-                    "character_id": other_id,
-                    "character": db.characters[other_id].name,
-                    "gender": db.characters[other_id].gender,
-                    "number_of_lines_together": lines,
-                }
-                for other_id, lines in get_top_conv_characters(character)
-            ),
-        }
-        return result
-
-    raise HTTPException(status_code=404, detail="character not found.")
+#     stmt = (
+#         sqlalchemy.select()
+#         .limit(limit)
+#         .offset(offset)
+#         .order_by(order_by, db.movies.c.movie_id)
+#     )
+#     raise HTTPException(status_code=404, detail="character not found.")
 
 
-class character_sort_options(str, Enum):
-    character = "character"
-    movie = "movie"
-    number_of_lines = "number_of_lines"
+# class character_sort_options(str, Enum):
+#     character = "character"
+#     movie = "movie"
+#     number_of_lines = "number_of_lines"
 
 
 @router.get("/characters/", tags=["characters"])
@@ -82,7 +49,7 @@ def list_characters(
     name: str = "",
     limit: int = Query(50, ge=1, le=250),
     offset: int = Query(0, ge=0),
-    sort: character_sort_options = character_sort_options.character,
+    # sort: character_sort_options = character_sort_options.character,
 ):
     """
     This endpoint returns a list of characters. For each character it returns:
@@ -105,6 +72,8 @@ def list_characters(
     maximum number of results to return. The `offset` query parameter specifies the
     number of results to skip before returning results.
     """
+
+    conn = engine.connect()
 
     if name:
         def filter_fn(c):
--- src/database.py
+++ src/database.py
@@ -1,118 +1,27 @@
-import csv
-from src.datatypes import Character, Movie, Conversation, Line
+import sqlalchemy
 import os
-import io
-from supabase import Client, create_client
 import dotenv
 
-# DO NOT CHANGE THIS TO BE HARDCODED. ONLY PULL FROM ENVIRONMENT VARIABLES.
-dotenv.load_dotenv()
-supabase_api_key = os.environ.get("SUPABASE_API_KEY")
-supabase_url = os.environ.get("SUPABASE_URL")
+def database_connection_url():
+    dotenv.load_dotenv()
+    DB_USER: str = os.environ.get("POSTGRES_USER")
+    DB_PASSWD = os.environ.get("POSTGRES_PASSWORD")
+    DB_SERVER: str = os.environ.get("POSTGRES_SERVER")
+    DB_PORT: str = os.environ.get("POSTGRES_PORT")
+    DB_NAME: str = os.environ.get("POSTGRES_DB")
+    return f"postgresql://{DB_USER}:{DB_PASSWD}@{DB_SERVER}:{DB_PORT}/{DB_NAME}"
 
-if supabase_api_key is None or supabase_url is None:
-    raise Exception(
-        "You must set the SUPABASE_API_KEY and SUPABASE_URL environment variables."
-    )
+# Create a new DB engine based on our connection string
+engine = sqlalchemy.create_engine(database_connection_url())
 
-supabase: Client = create_client(supabase_url, supabase_api_key)
+# Use reflection to derive table schema. You can also code this in manually.
+metadata_obj = sqlalchemy.MetaData()
+movies = sqlalchemy.Table("movies", metadata_obj, autoload_with=engine)
 
-sess = supabase.auth.get_session()
+lines = sqlalchemy.Table("lines", metadata_obj, autoload_with=engine)
 
-# Reading in the log file from the supabase bucket
-logs = []
-conv_id = 0
+characters = sqlalchemy.Table("characters", metadata_obj, autoload_with=engine)
 
-def update_log():
-    log_csv = (
-        supabase.storage.from_("movie-api")
-        .download("conversations.csv")
-        .decode("utf-8")
-    )
+conversations = sqlalchemy.Table("conversations", metadata_obj, autoload_with=engine)
 
-    for row in csv.DictReader(io.StringIO(log_csv), skipinitialspace=True):
-        logs.append(row)
-
-
-# Writing to the log file and uploading to the supabase bucket
-def upload_new_log():
-    output = io.StringIO()
-    csv_writer = csv.DictWriter(
-        output, fieldnames=["conversation_id", "character1_id",
-                            "character2_id", "movie_id"]
-    )
-    csv_writer.writeheader()
-    csv_writer.writerows(logs)
-    supabase.storage.from_("movie-api").upload(
-        "conversations.csv",
-        bytes(output.getvalue(), "utf-8"),
-        {"x-upsert": "true"},
-    )
-
-
-def try_parse(type, val):
-    try:
-        return type(val)
-    except ValueError:
-        return None
-
-
-with open("movies.csv", mode="r", encoding="utf8") as csv_file:
-    movies = {
-        try_parse(int, row["movie_id"]): Movie(
-            try_parse(int, row["movie_id"]),
-            row["title"] or None,
-            row["year"] or None,
-            try_parse(float, row["imdb_rating"]),
-            try_parse(int, row["imdb_votes"]),
-            row["raw_script_url"] or None,
-        )
-        for row in csv.DictReader(csv_file, skipinitialspace=True)
-    }
-
-with open("characters.csv", mode="r", encoding="utf8") as csv_file:
-    characters = {}
-    for row in csv.DictReader(csv_file, skipinitialspace=True):
-        char = Character(
-            try_parse(int, row["character_id"]),
-            row["name"] or None,
-            try_parse(int, row["movie_id"]),
-            row["gender"] or None,
-            try_parse(int, row["age"]),
-            0,
-        )
-        characters[char.id] = char
-
-update_log()
-with open("conversations.csv", mode="r", encoding="utf8") as csv_file:
-    conversations = {}
-    for row in csv.DictReader(csv_file, skipinitialspace=True):
-        conv = Conversation(
-            try_parse(int, row["conversation_id"]),
-            try_parse(int, row["character1_id"]),
-            try_parse(int, row["character2_id"]),
-            try_parse(int, row["movie_id"]),
-            0,
-        )
-        conversations[conv.id] = conv
-        conv_id = conv.id + 1
-
-with open("lines.csv", mode="r", encoding="utf8") as csv_file:
-    lines = {}
-    for row in csv.DictReader(csv_file, skipinitialspace=True):
-        line = Line(
-            try_parse(int, row["line_id"]),
-            try_parse(int, row["character_id"]),
-            try_parse(int, row["movie_id"]),
-            try_parse(int, row["conversation_id"]),
-            try_parse(int, row["line_sort"]),
-            row["line_text"],
-        )
-        lines[line.id] = line
-        c = characters.get(line.c_id)
-        if c:
-            c.num_lines += 1
-
-        conv = conversations.get(line.conv_id)
-        if conv:
-            conv.num_lines += 1
+print(movies.columns)
